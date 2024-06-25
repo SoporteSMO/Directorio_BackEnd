e21a3e8706770266bc2f2a8915f3920eb383e5c8
DesarrolloSMO <desarrollo@smo.ec>
2024-06-25 10:31:32 -0500

    Listando Publicidades

--- src/controller/publicidadesController.js
+++ src/controller/publicidadesController.js
@@ -1,5 +1,17 @@
+import Publicidades from "../models/Publicidades.js";
 const listarPublicidades = async (req, res) => {
-    res.status(200).json({ msg: "hola" });
+    try {
+        const publicidades = await Publicidades.findAll();
+        console.log(publicidades);
+        if (publicidades.length === 0) {
+            const error = new Error("No tienes publicidades registradas");
+            return res.status(404).json({ msg: error.message });
+        }
+        return res.status(200).json(publicidades);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error al obtener las capacitaciones" });
+    }
 };
 
 export { listarPublicidades };
